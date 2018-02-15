036b70f235a13c7f9c6a38eecd2d3f57a9aeefe2
Milena Makaveeva <ga92yup@mytum.de>
2018-02-15 13:58:21 +0100

    Fix

--- src/main/java/tum/sebis/apm/repository/SprintTeamRepository.java
+++ src/main/java/tum/sebis/apm/repository/SprintTeamRepository.java
@@ -17,5 +17,6 @@ public interface SprintTeamRepository extends MongoRepository<SprintTeam, String
 
     List<SprintTeam> findBySprintAndTeam(Iteration sprint, Team team);
     List<SprintTeam> findBySprintOrderByTeam(Iteration sprint);
+    List<SprintTeam> findBySprint(Iteration sprint);
     List<SprintTeam> findByTeam (Team team);
 }
